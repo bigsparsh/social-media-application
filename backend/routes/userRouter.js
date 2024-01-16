44f2bb72990b562051ece7ccff95dee09b59f101
bigsparsh <sparshsingh7586@gmail.com>
2024-01-16 11:15:50 +0530

    updated upload location

--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -6,7 +6,7 @@ const router = Router();
 const multer = require("multer");
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, "../frontend/public/uploads/");
+		cb(null, "../uploads/");
 	},
 	filename: function (req, file, cb) {
 		const uniqueSuffix =
